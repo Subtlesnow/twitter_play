4474328a1ed723d8f5aaa44518328e8cfd79bbb3
Steven VanWoerkom <stevenvanwoerkom@Stevens-MacBook-Pro.local>
2016-07-22 16:35:47 -0400

    Testing mongo connect

--- scripts/pull_twitter.py
+++ scripts/pull_twitter.py
@@ -1,12 +1,17 @@
 import tweepy
 import sys
-
+import pymongo
+import json
 sys.path.append("../")
 from import_config import load_config
+from pymongo import MongoClient
 
 config = load_config()
 
-# print(config)
+client = MongoClient(config["database"]["connection_url"])
+
+db = client[config['database']['database_name']]
+conn = db[config['database']['collection_name']]
 
 auth = tweepy.OAuthHandler(config["twitter"]["consumer_key"], config["twitter"]["consumer_secret"])
 auth.set_access_token(config["twitter"]["access_key"], config["twitter"]["access_secret"])
@@ -19,5 +24,6 @@ for tweet in new_tweets:
 	extracted_data["text"] = tweet.text.encode("utf-8")
 	extracted_data["created_at"] = tweet.created_at
 	extracted_data["twitter_id"] = tweet.id_str
+	conn.insert_one(extracted_data)
 	print(extracted_data)
 
